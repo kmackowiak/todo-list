b90fa7330f237e0c7a87a2f4788b07cca40a8bb0
kmackowiak <kamilm92@gmail.com>
2018-06-27 21:10:25 +0200

    Added delete all button and fixed problems with local storage

--- js/script.js
+++ js/script.js
@@ -1,18 +1,81 @@
 
 
-function createObj() {
-    var itemsId = 0;
-    var itemsId = document.getElementsByClassName("list-group-item").length;
 
 
-    const ob = {
-        'taskText': this.writedText,
-        'taskPriority': taskPriority.value,
-    };
-    localStorage.setItem(itemsId, JSON.stringify(ob));
-    const retrievedObject = localStorage.getItem(itemsId);
+
+var countItems = localStorage.length;
+
+
+function loadItems (){
+
+    //console.log(countItems);
+    for (i = 0; i < countItems; i++)
+    {
+        var retrievedItem = localStorage.getItem(i);
+        // console.log(retrievedItem);
+        if (retrievedItem == null)
+        {
+            countItems++;
+            continue
+        }
+        retrievedItemParse = (JSON.parse(retrievedItem));
+
+
+        var retWritedText = retrievedItemParse.writedText;
+        var retTaskPriority = retrievedItemParse.taskPriority;
+
+
+        // console.log(retWritedText);
+
+        // if (retWritedText != null && retTaskPriority != null)
+        //    {
+        createListItem(retWritedText, retTaskPriority);
+
+
+        //console.log(i);
+        //    }
+
+
+    }
+
+
 }
 
+
+
+
+function createObj() {
+    // var itemsId = 0;
+    // var itemsId = document.getElementsByClassName("list-group-item").length;
+    var itemsId = localStorage.length;
+    var maxKey = 0;
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var key = parseInt(key);
+        if (key >= maxKey)
+        {
+            var maxKey = key + 1;
+        }
+        // console.log("key", key, " maxkey", maxKey);
+
+    }
+
+
+
+
+    // items id usawic na ostatnie id w local strage
+    console.log("aktaulne", maxKey);
+
+    const ob = {
+        'writedText': this.writedText,
+        'taskPriority': taskPriority.value,
+    };
+
+    if (this.writedText != null && taskPriorityValue != null) {
+        localStorage.setItem(maxKey, JSON.stringify(ob));
+        return maxKey;
+    }
+}
 
 
 function getText() {
@@ -65,10 +128,14 @@ function createListItem(writedText, taskPriorityValue) {
     taskText.classList.remove("red-border");
     taskPriority.classList.remove("red-border");
 
+
     var list = document.getElementById("list");
 
     //    Create blank list item
     var listItem = document.createElement("li");
+
+    // listItem.setAttribute("id", localStorage.length);
+
 
     //    Add classes for list item
     listItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
@@ -97,13 +164,23 @@ function createListItem(writedText, taskPriorityValue) {
     listItemButton.setAttribute("aria-label", "Close");
     var listItemButtonSpan = document.createElement("span");
     listItemButton.appendChild(listItemButtonSpan);
+    var addId = document.querySelectorAll('.list-group-item').length;
+
+    listItemButton.setAttribute("id", addId);
+
+
     listItemButtonSpan.innerHTML = "&times;";
     listItemButtonSpan.setAttribute("aria-hidden", "True");
 
     // Add event for delete list item
     listItemButton.addEventListener("click", function() {
         if (confirm("Do you want delete this task?")) {
+
+            //  console.log("dupa", listItemButton.id);
             listItem.remove();
+
+            localStorage.removeItem(listItemButton.id);
+
         }
     });
 
@@ -124,5 +201,34 @@ function createListItem(writedText, taskPriorityValue) {
 
 }
 
-createObj();
+
+function clearList(){
+
+
+
+    var listItems = document.getElementById("list");
+
+
+        clearAll = document.getElementById("clearAll");
+
+
+    clearAll.addEventListener("click", function(){
+        if (confirm("Do you want delete all tasks?")) {
+
+            listItems.innerHTML = "";
+            localStorage.clear();
+
+        }
+
+
+    });
+
+
+
+}
+clearList()
+
+
+loadItems();
+//createObj();
 getText();
